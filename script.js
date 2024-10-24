f5b0c992a0fa0b0a21e7090d9f495d8ceeacf5a9
K.Durga Bhavani <bhavanidurga759@gmail.com>
2024-10-24 20:09:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,143 @@
+//conditional statements
+
+// var a=22 //if statements
+// if(a>=23){
+//     console.log("hello friends")
+// }else{
+//     console.log("how are you")
+// }
+     
+// var a=25
+// if(a<=23){
+//     console.log("hello guys")
+// }else{
+//     console.log("what are doing")
+// }
+
+// var a_married=true; //if else statement
+// if(a_married){
+//     console.log("I am very happy in life")
+// }else{
+//     console.log("i am enjoy my self")
+// }
+
+// var a_employe;
+// if(a_employe=false){
+//     console.log("given salary")
+// }else{
+//     console.log("not given salary")
+// }
+
+// var day="Thursday"; //if..else..if...else
+// if(day==="monday"){
+//     console.log("this is monday");
+// }else if(day==="Tuesday"){
+//     console.log("this is Thuesday")
+//     }else if(day==="wednesday"){
+//         console.log("this is wednesday")
+//     }else if(day==="Thursday"){
+//         console.log("this is thursday")
+//     }else if(day==="friday"){
+//         console.log("this is friday")
+//     }else if(day==="Sartday"){
+//         console.log("this is sartday")
+//     }else{
+//         console.log("weekend")
+//     }
+
+//     let fruit = "apple"; //switch case
+//     switch (fruit) {
+//       case "banana":
+//         console.log("It's a banana.");
+//         break;
+//       case "apple":
+//         console.log("It's an apple.");
+//         break;
+//       default:   
+//         console.log("Unknown fruit.");
+//     }
+
+    //String methods
+        //   var a; //declarartion
+        //  var a="bhavani" //intialization
+        //  b="durga" //assign
+
+        //  let a; //declaration
+        //  let a="sravani";//intilalization
+        //  let a="sravani";
+        //  b="bhavani";
+        //  b="bhavani";
+
+       
+        //  c=a.concat(b)
+        //  console.log(c)
+        //  console.log(c.length)
+        //  console.log(c.charAt(10))
+
+        // let arr=[1,2,3,4,5];
+        // arr.length=3;
+        // console.log(arr);
+        // arr.length=5;
+        // console.log(arr);
+
+        // console.log(typeof typeof 1);
+        // console.log(typeof NaN);
+        // console.log(typeof null);
+        // console.log(typeof undefined);
+        
+        // let a=[1,2,3];
+        // let b=[1,2,3];
+        // console.log( a==b);
+        // console.log( a===b);
+        // console.log([]==[]);
+        // console.log([] === []);
+
+        // function foo(a,b,c){
+        //     arguments[0]=10;
+        //     console.log(a, arguments[0]);
+        // }
+        // foo(1,2,3);
+
+        // let greeting ="hello";
+        // let name ="John";
+        // let message=greeting+","+ name +"!";
+        // let anotherMessage=greeting.concat(",",name,"!")
+        // console.log(anotherMessage);
+
+        // let Intial="Kalisetti";
+        // let name="Bhavani";
+        // let concating=Intial+","+ name +"!";
+        // let anotherconcat=Intial.concat(",",name,"!")
+        // console.log(anotherconcat);
+
+        // var text="Hello,world!";
+        // var length=text.length;
+        // console.log(length)
+
+        // var str="Hello";
+        // var firstchar=str[1]
+        // console.log(firstchar);
+
+        // var str="Hello,World!"
+        // console.log(str.charAt(6))
+
+        // var str=" Hello,world!";
+        // cpnsole.log(str.at(1));
+
+
+        // function reverse(str){
+        //     result=""
+        //     for(i=str.length-1;i>=0;i--){
+        //         result+=str[i]
+        //     }
+        //     // console.log(str[i])
+        //     console.log(result)
+        //     return result;
+        // }
+        // var userinput=prompt+("Enter the value")
+        // const output=reverse (userinput)
+        // console.log(output)
+
+        
+
+        
